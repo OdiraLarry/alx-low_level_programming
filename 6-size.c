d8d569eae7100e36e69052fc861562aad2b23c0f
OdiraLarry <odiralarry@gmail.com>
2023-02-20 09:34:10 -0800

    Task 4

---
+++ 6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the size of various computer types
+ * Return 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+	int b;
+	long int;
+	long long int;
+	float f;
+
+
+        printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));               
+}
