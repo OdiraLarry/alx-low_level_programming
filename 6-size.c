706121f41ac0b24133ca66ea776e3ef6e03e2cb2
OdiraLarry <odiralarry@gmail.com>
2023-02-20 11:53:24 -0800

    Task 6

--- 6-size.c
+++ 6-size.c
@@ -3,7 +3,6 @@
  * main - A program that prints the size of various computer types
  * Return 0 (Success)
  */
-
 int main(void)
 {
 	char a;
@@ -12,6 +11,11 @@ int main(void)
 	long long int;
 	float f;
 
-
-        printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));               
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a)); 
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(e));
+return (0);
 }
